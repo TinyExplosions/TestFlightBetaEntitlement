e2588aff55b7a5f41c7c23355202e23e3215ea79
Al Graham <alastair.graham@feedhenry.com>
2015-04-22 09:35:21 +0100

    testing out hooks

--- TestEntitlement/hooks/install_entitlements_plist.js
+++ TestEntitlement/hooks/install_entitlements_plist.js
@@ -1,101 +1,39 @@
 #!/usr/bin/env node
 
-var xcode = require('xcode');
+// this plugin replaces arbitrary text in arbitrary files
+//
+// Look for the string CONFIGURE HERE for areas that need configuration
+//
+
 var fs = require('fs');
 var path = require('path');
 
-var createEntitlementsPlistFile = function (appId) {
-  return '<?xml version="1.0" encoding="UTF-8"?>\n' +
-  '<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">\n' +
-  '<plist version="1.0">\n' +
-  '    <dict>\n' +
-  '        <key>keychain-access-groups</key>\n' +
-  '        <array>\n' +
-  '        <string>Testing-This-Out</string>\n' +
-  '        </array>\n' +
-  '    </dict>\n' +
-  '</plist>\n';
+var rootdir = process.argv[2];
 
-};
+function replace_string_in_file(filename, to_replace, replace_with) {
+    var data = fs.readFileSync(filename, 'utf8');
 
-module.exports = function(context) {
-  // console.log("*******");
-  // console.log(context);
-  // console.log("*******");
+    var result = data.replace(new RegExp(to_replace, "g"), replace_with);
+    fs.writeFileSync(filename, result, 'utf8');
+}
 
-  var Q = context.requireCordovaModule('q');
-  var deferral = new Q.defer();
-
-  if(context.opts.cordova.platforms.indexOf('ios') < 0){
-    throw new Error('This plugin expects the ios platform to exist.');
-  }
-
-  var iosFolder = path.join(context.opts.projectRoot, 'platforms/ios/');
-
-  fs.readdir(iosFolder, function (err, data) {
-    if(err) {
-      throw err;
-    }
-
-    var projFolder;
-    var projName;
-
-    // Find the project folder by looking for *.xcodeproj
-    if(data && data.length) {
-      data.forEach(function (folder) {
-        if(folder.match(/\.xcodeproj$/)) {
-          projFolder = path.join(iosFolder, folder);
-          projName = path.basename(folder, '.xcodeproj');
+if (rootdir) {
+    // CONFIGURE HERE
+    // with the names of the files that contain tokens you want replaced.  Replace files that have been copied via the prepare step.
+    var filestoreplace = [
+        // ios
+        "platforms/ios/www/index.html",
+    ];
+    filestoreplace.forEach(function(val, index, array) {
+        var fullfilename = path.join(rootdir, val);
+        if (fs.existsSync(fullfilename)) {
+            // CONFIGURE HERE
+            // with the names of the token values. For example, below we are looking for the token /*REP*/ 'api.example.com' /*REP*/ and will replace that token
+            replace_string_in_file(fullfilename, "/\\*REP\\*/ 'api.example.com' /\\*REP\\*/", "/\\*REP\\*/ 'api.thisworks.com' /\\*REP\\*/");
+            // ... any other configuration
+        } else {
+            //console.log("missing: "+fullfilename);
         }
-      });
-    }
-
-    if(!projFolder) {
-      throw new Error("Could not find an .xcodeproj folder in: " + iosFolder);
-    }
-
-    // Look up the directory tree for the config.xml
-    var configFilePath = path.join(__dirname, '../../../config.xml');
-    if(!fs.existsSync(configFilePath)){
-      throw new Error("Expected to find the project's config.xml here: " + configFilePath);
-    }
-
-    var configXml = fs.readFileSync(configFilePath).toString();
-
-    // Find the 'widget id="com.myapp.id"'
-    var myRegexp = /widget(.*)id="(.*?)"/;
-    var match = myRegexp.exec(configXml);
-    match = myRegexp.exec(configXml);
-    var appId = match[2];
-    //console.log(configXml);
-
-    // create a new entitlements plist file
-    var entitlementsPlistFile = path.join(iosFolder, projName, "Resources/Entitlements.plist");
-
-    var plistFileContents = createEntitlementsPlistFile(appId);
-
-    fs.writeFile(entitlementsPlistFile, plistFileContents, function (err) {
-      if(err) {
-        throw err;
-      }
-
-
-      var projectPath = path.join(projFolder, 'project.pbxproj');
-      var xcodeProject = xcode.project(projectPath);
-
-      // parsing is async, in a different process
-      xcodeProject.parse(function (err) {
-        xcodeProject.addResourceFile('Entitlements.plist');
-
-        fs.writeFileSync(projectPath, xcodeProject.writeSync());
-        console.log('Added Entitlements.plist to the xcode project');
-
-        deferral.resolve();
-      });
-
     });
 
-  });
-
-  return deferral.promise;
-};
+}
